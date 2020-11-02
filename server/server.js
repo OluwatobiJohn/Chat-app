caaf3aa693333b78e1dd06e129dc92fd4dec6966
DELL <you@example.com>
2020-11-02 14:15:40 +0100

    Connection and Disconnection event

--- server/server.js
+++ server/server.js
@@ -1,11 +1,25 @@
 const express = require('express');
-const app = express();
+const webSocket = require('socket.io');
 const path = require('path');
+const http = require('http');
+
 const publicPath = path.join(__dirname, '../Public');
+let app = express();
+let server = http.createServer(app);
+let io = webSocket(server);
+
 
 app.use(express.static(publicPath));
 
+io.on('connection', (socket) => {
+    console.log('New User Connected');
 
-app.listen(3000, () => {
+    socket.on('disconnect', () => {
+        console.log('Client disconnected');
+    })
+})
+
+
+server.listen(3000, () => {
     console.log('App is running on port 3000');
 })
